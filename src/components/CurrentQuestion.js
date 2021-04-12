fd4663150f8d1b23eb863e3531f7a2ecf807bf3d
Sofarz <sofia.wallen@gmail.com>
2021-04-12 11:50:59 +0200

    adding changes from codealong

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { quiz } from './reducers/quiz'
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const quizOver = useSelector((state) => state.quiz.quizOver)
+  // we can write it store instead of state(optional)
+
+  const dispatch = useDispatch()
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
@@ -11,6 +16,9 @@ export const CurrentQuestion = () => {
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
+      <button onClick={() => dispatch(quiz.actions.goToNextQuestion())} type="button">
+        Go to next question
+      </button>
     </div>
   )
 }
--- src/reducers/quiz.js
+++ src/reducers/quiz.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-// Change these to your own questions!
+// Change these to your own questions! store ALL questions in quiz here!
 const questions = [
   { id: 1, questionText: 'Who set the Olympic record for the 100m dash in 2012?', options: ['Usain Bolt', 'Justin Gatlin', 'Tyson Gay', 'Asafa Powell'], correctAnswerIndex: 0 },
   { id: 2, questionText: 'When was Michael Phelps last named male World Swimmer of the Year?', options: ['2012', '2014', '2016', '2018'], correctAnswerIndex: 2 }
